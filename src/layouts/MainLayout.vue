62c0837dd7e8bdcdae60834ef356fd404e6db69d
crom0805 <crom0805@gmail.com>
2021-10-29 17:04:03 +0900

    init

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="hd-blue">
+    <q-header class="hd-blue" bordered>
       <q-toolbar>
         <q-btn flat dense round @click="toggleLeftDrawer" icon="menu" aria-label="Menu" />
         <q-toolbar-title>
@@ -8,8 +8,19 @@
         </q-toolbar-title>
         <q-space/>
         <div class="q-gutter-sm row items-center no-wrap">
+          <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="~/assets/images/boy-avatar.png">
+            </q-avatar>
+            <q-tooltip class="bg-accent" >User Profile</q-tooltip>
+          </q-btn>
           <q-btn round dense flat color="black" :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
                  @click="$q.fullscreen.toggle()" v-if="$q.screen.gt.sm">
+            <q-tooltip class="bg-accent">Maximize</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat color="black" icon="logout"
+                 @click="$q.fullscreen.toggle()">
+            <q-tooltip class="bg-accent">Logout</q-tooltip>
           </q-btn>
         <!-- notifications area
           <q-btn round dense flat color="blue" icon="notifications">
@@ -25,18 +36,13 @@
               </q-list>
             </q-menu>
           </q-btn>-->
-          <q-btn round flat>
-            <q-avatar size="26px">
-              <img src="~/assets/images/boy-avatar.png">
-            </q-avatar>
-          </q-btn>
         </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered :width="250">
       <q-scroll-area dark class="fit menu-blue">
-        <q-item to="/Dashboard" clickable :active="link === 'Dashboard'" @click="link = 'Dashboard'" active-class="menu-link">
+        <q-item to="/Dashboard" :active="link === 'Dashboard'" @click="link = 'Dashboard'" active-class="menu-link">
           <q-item-section avatar>
             <q-icon name="dashboard"/>
           </q-item-section>
@@ -44,7 +50,7 @@
             <q-item-label>Dashboard</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item to="/Dashboard2" clickable :active="link === 'Dashboard2'" @click="link = 'Dashboard2'" active-class="menu-link">
+        <q-item to="/Dashboard2" :active="link === 'Dashboard2'" @click="link = 'Dashboard2'" active-class="menu-link">
           <q-item-section avatar>
             <q-icon name="dashboard"/>
           </q-item-section>
@@ -53,8 +59,8 @@
           </q-item-section>
         </q-item>
         <q-expansion-item icon="person" label="User" default-opened>
-          <q-list class="q-pl-lg menu-dt-blue">
-            <q-item to="/UserList" clickable :active="link === 'UserList'" @click="link = 'UserList'" active-class="menu-link">
+          <q-list class="menu-dt-blue">
+            <q-item class="menu-dt-item" to="/UserList" :active="link === 'UserList'" @click="link = 'UserList'" active-class="menu-link">
               <q-item-section avatar>
                 <q-icon name="list"/>
               </q-item-section>
@@ -62,7 +68,7 @@
                 <q-item-label>User List</q-item-label>
               </q-item-section>
             </q-item>
-            <q-item to="/UserSignup" clickable :active="link === 'UserSignup'" @click="link = 'UserSignup'" active-class="menu-link">
+            <q-item class="menu-dt-item"  to="/UserSignup" :active="link === 'UserSignup'" @click="link = 'UserSignup'" active-class="menu-link">
               <q-item-section avatar>
                 <q-icon name="manage_accounts"/>
               </q-item-section>
