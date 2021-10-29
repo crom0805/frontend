67ef9bfa12e4d5072cffd39be3bb976b171f5304
crom0805 <crom0805@gmail.com>
2021-10-29 17:26:47 +0900

    layout add class

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header class="hd-blue" bordered>
+  <q-layout view="lHh Lpr lFf" class="fixed fit scroll">
+    <q-header class="hd-blue" bordered >
       <q-toolbar>
         <q-btn flat dense round @click="toggleLeftDrawer" icon="menu" aria-label="Menu" />
         <q-toolbar-title>
@@ -22,20 +22,6 @@
                  @click="$q.fullscreen.toggle()">
             <q-tooltip class="bg-accent">Logout</q-tooltip>
           </q-btn>
-        <!-- notifications area
-          <q-btn round dense flat color="blue" icon="notifications">
-            <q-badge color="red" text-color="white" floating>
-              5
-            </q-badge>
-            <q-menu>
-              <q-list style="min-width: 100px">
-                <messages></messages>
-                <q-card class="text-center no-shadow no-border">
-                  <q-btn label="View All" style="max-width: 120px !important;" flat dense class="text-indigo-8"></q-btn>
-                </q-card>
-              </q-list>
-            </q-menu>
-          </q-btn>-->
         </div>
       </q-toolbar>
     </q-header>
